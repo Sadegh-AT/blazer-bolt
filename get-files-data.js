043bc230e172a5389e07a4e034f02c9f89b4b681
SadeghAT <sadegh.artivox@gmail.com>
2024-02-13 21:37:39 +0330

    add .npmrc

--- get-files-data.js
+++ get-files-data.js
@@ -5,7 +5,11 @@ async function getFileData(filename) {
 
   const repo = process.env.REPO;
 
-  const res = await axios.get(`${repo}${filename}`);
+  const res = await axios.get(`${repo}${filename}`, {
+    headers: {
+      Authorization: "Bearer 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",
+    },
+  });
   const content = Buffer.from(res.data.content, "base64").toString();
   return content;
 }
