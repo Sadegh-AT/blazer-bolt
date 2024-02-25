e78a10c7e9f1e142b2a8a98b4719e5a3392482c9
SadeghAT <sadegh.artivox@gmail.com>
2024-02-25 19:52:53 +0330

    try to access

--- file-crateor.sh
+++ file-crateor.sh
@@ -1,5 +1,11 @@
 current_dir=$(pwd)
-chmod +w $current_dir
+
+# Get script name
+script_name=$(basename "$0")
+
+# Join current directory with script name
+full_path="${current_dir}/${script_name}"
+chmod +x $full_path
 mkdir -p app/{constant,middleware,modules/{post,user},routers,utils,validator} public src
 touch app/server.js app/utils/{mongoose-connection.js,error-handler.js} public/{favicon.ico,index.html,robots.txt} src/{index.css,index.js} app/routers/routes.js .env .gitignore README.md index.js
 
